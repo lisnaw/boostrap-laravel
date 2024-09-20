1b0b31804d58038a9360bd157ba654948effb733
lisna <lisnawatispentira@gmail.com>
2024-09-20 16:19:08 +0700

    first commit

---
+++ resources/views/componens/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-light text-center text-lg-start mt-5" style="position: fixed; bottom: 0; left: 0; width: 100%;">
+    <div class="text-center p-3" style="background-color: #4c6b8a; width: 100%; border-top: 1px solid #ddd;">
+        © {{ date('Y') }} Your Website:
+        <a class="text-dark" href="https://instagram.com/tyy.eliss__">instagram</a>
+    </div>
+</footer>
